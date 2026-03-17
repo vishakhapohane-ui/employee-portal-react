8e347b6d1809bf9218621f1b629cc481d439d125
vishakha-pohane_ola <vishakha.pohane@olacabs.com>
2026-03-17 15:34:03 +0530

    Fix router basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
